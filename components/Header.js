bcb50c7dbc07eca2b02b9f11698f47700d4f0a2c
Vikram Mehta <vikrammehta83@gmail.com>
2023-04-05 09:57:23 -0400

    Setup Next js and Tailwind css. Created a Header component and added Login and Logout buttons. Added the read me file. First commit

---
+++ components/Header.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Header = () => {
+    return (
+        <header className='flex justify-between items-center'>
+            <span className='text-2xl font-bold text-gray-700'>My Notes</span>
+            <nav className='flex'>
+                <a href='/api/login' className='block rounded bg-blue-500 hover:bg-blue-600 text-white px-4 py-2'>Login</a>
+                <a href='/api/logout' className='block rounded bg-orange-500 hover:bg-orange-600 text-white px-4 py-2 ms-2'>Logout</a>
+            </nav>
+        </header>
+    )
+}
+
+export default Header
--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,11 @@
 import '@/styles/globals.css'
+import Header from '@/components/Header'
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <div className="container mx-auto py-6">
+      <Header />
+      <Component {...pageProps} />
+    </div>
+  )
 }
